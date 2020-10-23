c5ca966499b37377b58e33f0dd26a7f3e18ab908
orvice <orvice@gmail.com>
2020-10-23 15:21:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ cd /tmp
 echo "Install Python"
 apt-get update
 apt-get -y upgrade
-apt-get install -y software-properties-common  make gcc g++
+
+apt-get install -y make gcc g++
+
+# apt-get install -y software-properties-common
+
 add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y python3.6
